cfb0837ad03285bb835f16d7729cae40ea305411
Rafael de Souza Baptista <faelbaptista0@gmail.com>
2025-01-22 22:34:28 -0300

    src

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
+import api from "./src/services/api";
 
 export default function App() {
+  const [cep, setCep] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={StyleSheet.container}>
+      <View style={{ alignItems: "center" }}>
+        <Text style={styles.text}>Digite seu CEP aqui:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Ex:74737271"
+          value={cep}
+          onChangeText={(texto) => setCep(texto)}
+          keyboardType="numeric"
+        />
+      </View>
+
+      <View style={styles.areaBtn}>
+        <TouchableOpacity style={styles.botao}>
+          <Text style={styles.botaoText}>Buscar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.botao, { backgroundColor: "#cd3e1d" }]}
+        >
+          <Text style={styles.botaoText}>Limpar</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.resultado}>
+        <Text style={styles.itemText}>CEP:23452342</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input: {
+    backgroundColor: "#6643",
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 5,
+    width: "90%",
+    padding: 10,
+    fontSize: 18,
+  },
+  text: {
+    marginTop: 40,
+    marginBottom: 15,
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  areaBtn: {
+    alignItems: "center",
+    flexDirection: "row",
+    marginTop: 15,
+    justifyContent: "space-around",
+  },
+
+  botao: {
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    borderRadius: 5,
+    backgroundColor: "#728119",
+  },
+  botaoText: {
+    fontSize: 22,
+    color: "white",
   },
 });
